74070e6bd06dcb9e338d49d34b8b4d997d0a9164
itman1000 <itman202043@gmail.com>
2023-08-25 14:46:24 +0900

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livewire Test</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+<body>
+    <main>
+        <a href="{{ route('counter') }}">Go to Counter</a>
+    </main>
+
+    @livewireScripts
+</body>
+</html>
